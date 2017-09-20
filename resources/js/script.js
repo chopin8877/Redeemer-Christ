a70edb9a3c71927630dd33758ef3f1b52e23f2c4
chopin8877 <31151578+chopin8877@users.noreply.github.com>
2017-09-21 06:10:47 +0900

    animation effect

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,7 +1,8 @@
 $(document).ready(function() {
-    
-    
-    /* For the sticky navigation */
+
+
+    /* For the sticky navigation when put .class js--section-features in html,
+    logo-black exactly appears where it should be*/
     $('.js--section-features').waypoint(function(direction) {
         if (direction == "down") {
             $('nav').addClass('sticky');
@@ -11,19 +12,19 @@ $(document).ready(function() {
     }, {
       offset: '60px;'
     });
-    
-    
-    /* Scroll on buttons */
+
+
+    /* Scroll on buttons if you want composition to go to SoundCloud, target="" */
     $('.js--scroll-to-plans').click(function () {
-       $('html, body').animate({scrollTop: $('.js--section-plans').offset().top}, 1000); 
+       $('html, body').animate({scrollTop: $('.js--section-plans').offset().top}, 1000);
     });
-    
+
     $('.js--scroll-to-start').click(function () {
-       $('html, body').animate({scrollTop: $('.js--section-features').offset().top}, 1000); 
+       $('html, body').animate({scrollTop: $('.js--section-features').offset().top}, 1000);
     });
-    
-    
-    /* Navigation scroll */
+
+
+    /* Navigation scroll , anchor with id=""*/
     $(function() {
       $('a[href*=#]:not([href=#])').click(function() {
         if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
@@ -38,47 +39,47 @@ $(document).ready(function() {
         }
       });
     });
-    
-    
-    /* Animations on scroll */
+
+
+    /* Animations on scroll, shaking, fadein, fadeup, make class .js--wp-1*/
     $('.js--wp-1').waypoint(function(direction) {
         $('.js--wp-1').addClass('animated fadeIn');
     }, {
-        offset: '50%'
+        offset: '30%'
     });
-    
+
     $('.js--wp-2').waypoint(function(direction) {
         $('.js--wp-2').addClass('animated fadeInUp');
     }, {
         offset: '50%'
     });
-    
+
     $('.js--wp-3').waypoint(function(direction) {
         $('.js--wp-3').addClass('animated fadeIn');
     }, {
-        offset: '50%'
+        offset: '40%'
     });
-    
+
     $('.js--wp-4').waypoint(function(direction) {
         $('.js--wp-4').addClass('animated pulse');
     }, {
-        offset: '50%'
+        offset: '30%'
     });
-    
-    
+
+
     /* Mobile navigation */
     $('.js--nav-icon').click(function() {
         var nav = $('.js--main-nav');
         var icon = $('.js--nav-icon i');
-        
+
         nav.slideToggle(200);
-        
+
         if (icon.hasClass('ion-navicon-round')) {
             icon.addClass('ion-close-round');
             icon.removeClass('ion-navicon-round');
         } else {
             icon.addClass('ion-navicon-round');
             icon.removeClass('ion-close-round');
-        }        
+        }
     });
 });
