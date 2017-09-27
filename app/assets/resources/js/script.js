92debdf2dca2be02e22024e74de0f1846105270e
chopin8877 <31151578+chopin8877@users.noreply.github.com>
2017-09-28 07:07:23 +0900

    2nd page title should be fixed

---
+++ app/assets/resources/js/script.js
@@ -0,0 +1,109 @@
+$(document).ready(function() {
+
+
+    /* For the sticky navigation when put .class js--section-features in html,
+    logo-black exactly appears where it should be*/
+    $('.js--section-features').waypoint(function(direction) {
+        if (direction == "down") {
+            $('nav').addClass('sticky');
+        } else {
+            $('nav').removeClass('sticky');
+        }
+    }, {
+      offset: '60px;'
+    });
+
+
+    /* Scroll on buttons if you want composition to go to SoundCloud, target="" */
+    $('.js--scroll-to-plans').click(function () {
+       $('html, body').animate({scrollTop: $('.js--section-plans').offset().top}, 1000);
+    });
+
+    $('.js--scroll-to-start').click(function () {
+       $('html, body').animate({scrollTop: $('.js--section-features').offset().top}, 1000);
+    });
+
+
+    /* Navigation scroll , anchor with id=""*/
+    $(function() {
+      $('a[href*=#]:not([href=#])').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+          var target = $(this.hash);
+          target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+          if (target.length) {
+            $('html,body').animate({
+              scrollTop: target.offset().top
+            }, 1000);
+            return false;
+          }
+        }
+      });
+    });
+
+
+    /* Animations on scroll, shaking, fadein, fadeup, make class .js--wp-1*/
+    $('.js--wp-1').waypoint(function(direction) {
+        $('.js--wp-1').addClass('animated fadeIn');
+    }, {
+        offset: '30%'
+    });
+
+    $('.js--wp-2').waypoint(function(direction) {
+        $('.js--wp-2').addClass('animated fadeInUp');
+    }, {
+        offset: '50%'
+    });
+
+    $('.js--wp-3').waypoint(function(direction) {
+        $('.js--wp-3').addClass('animated fadeIn');
+    }, {
+        offset: '40%'
+    });
+
+    $('.js--wp-4').waypoint(function(direction) {
+        $('.js--wp-4').addClass('animated pulse');
+    }, {
+        offset: '30%'
+    });
+
+    $('.js--wp-5').waypoint(function(direction) {
+        $('.js--wp-5').addClass('animated zoomIn');
+    }, {
+        offset: '30%'
+    });
+
+
+    /* Mobile navigation */
+    $('.js--nav-icon').click(function() {
+        var nav = $('.js--main-nav');
+        var icon = $('.js--nav-icon i');
+
+        nav.slideToggle(200);
+
+        if (icon.hasClass('ion-navicon-round')) {
+            icon.addClass('ion-close-round');
+            icon.removeClass('ion-navicon-round');
+        } else {
+            icon.addClass('ion-navicon-round');
+            icon.removeClass('ion-close-round');
+        }
+    });
+
+ /* water effect for the second video*/
+ $('header').ripples({
+   resolution: 512,
+   dropRadius: 20,
+   perturbance: 0.04,
+});
+
+setInterval(function() {
+   var $el = $('header');
+   var x = Math.random() * $el.outerWidth();
+   var y = Math.random() * $el.outerHeight();
+   var dropRadius = 20;
+   var strength = 0.04 + Math.random() * 0.04;
+   $el.ripples('drop', x, y, dropRadius, strength);
+}, 400);
+setInterval(callback, delay);
+
+});
